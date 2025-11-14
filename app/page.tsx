a24ca64a7fab23cf24b469b3834c872f7a61cbed
TuBagus7 <tubagusdwifikri@gmail.com>
2025-11-15 06:52:41 +0700

    Updating at CSS - 'globals.css'

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,13 @@ import Link  from "next/link";
 export default function Home() {
   return (
     <>
-    <h1>Welcome to My Next.js App</h1>
+    <div className= "main-heading">
+    <h1>Welcome to My Portofolio</h1>
+    <p className="subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, officiis. Hic rerum aperiam odit atque iure suscipit, illo, sed ipsum ab quaerat nemo impedit rem, sint doloremque in laborum dicta.</p>
+    <br />
     <Link href="/about">Go to About Page</Link>
     <br />
+    </div>
     </>
   );
 }
