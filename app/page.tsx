fa6d1476f7891fd717884317c63be2ebde7eeae1
TuBagus7 <tubagusoseng@gmail.com>
2025-11-15 13:36:33 +0700

    Organize Code anda Add the API file in 'lib folder'

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
 import Image from "next/image";
-import Link  from "next/link";
+import Link from "next/link";
+import { getLastPost } from "@/app/lib/api";
+import PostList from "./components/PostList";
 
-
-
-export default function Home() {
+export default async function Home() {
+  const posts = await getLastPost();
   return (
     <>
-    <div className= "main-heading">
-    <h1>Welcome to My Portofolio</h1>
-    <p className="subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis, officiis. Hic rerum aperiam odit atque iure suscipit, illo, sed ipsum ab quaerat nemo impedit rem, sint doloremque in laborum dicta.</p>
-    </div>
+      <div className="main-heading">
+        <h1>Welcome to My Portofolio</h1>
+        <p className="subtitle">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis,
+          officiis. Hic rerum aperiam odit atque iure suscipit, illo, sed ipsum
+          ab quaerat nemo impedit rem, sint doloremque in laborum dicta.
+        </p>
+      </div>
+      <PostList posts={posts} />
     </>
   );
 }
