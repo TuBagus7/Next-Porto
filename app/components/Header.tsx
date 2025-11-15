5a23cf990f24b3bc12278a40ba8c1e89b721b8c6
TuBagus7 <tubagusoseng@gmail.com>
2025-11-15 10:08:09 +0700

    Add and Updating Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
 
-const Header = () =>{
-    return(
-        <header className="header">
-          <nav>
-            <div className="nav-links">
-              <Link href="/">Home</Link>
-              <Link href="/about">About</Link>
-            </div>
-          </nav>
-        </header>
-    )
-}
+const Header = () => {
+  return (
+    <header className="header">
+      <nav>
+        <div className="logo">
+          <Link href="/">Logo</Link>
+        </div>
+        <div className="nav-links">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
