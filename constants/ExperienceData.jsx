b1cb1d3710aa20cf3ace348a814457740cc973f2
TuBagus7 <tubagusoseng@gmail.com>
2025-11-28 16:59:36 +0700

    Update Experience name company not yet finshed

--- constants/ExperienceData.jsx
+++ constants/ExperienceData.jsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export const ExperienceData = [
   {
-    companyName: "KelasIoT",
+    companyName: "IoT Mentor",
     location: "Jakarta, Indonesia",
-    role: "IoT Mentor",
+    role: "KelasIoT",
     fromTo: "August 2024 - Present",
     description:
       "I help participants design and build simple IoT devices using sensors and communication modules like WiFi, MQTT, or HTTP. I also teach the basics of IoT and how to get devices online, help troubleshoot any technical issues they run into, and basically learn and explore the tech together with them.",
@@ -21,12 +21,12 @@ export const ExperienceData = [
     ),
   },
   {
-    companyName: "Project-Based intern ",
-    location: "Online",
-    role: "Frontend Developer Intern",
-    fromTo: "1 April - 1May 2023",
+    companyName: "Research Assistant",
+    location: "Riau, Indonesia",
+    role: "Islamic University of Riau",
+    fromTo: "June 2024 - January 2025",
     description:
-      "Completed a Final Project to create an e-commerce website UX using the Vue.js JavaScript Framework.",
+      "I assist lecturers with their research, handle assigned tasks professionally, and build project prototypes using microcontrollers. I also work on various Internet of Things projects and continue developing new ones along the way.",
     side: "right",
     icon: (
       <div className="relative w-[50px] h-[50px] rounded-full overflow-hidden border border-gray-200 bg-white flex items-center justify-center">
