4b4df7e6f0288e735884dd3ed466b7b3a45d21ae
TuBagus7 <tubagusoseng@gmail.com>
2025-12-01 16:51:57 +0700

    Updating ProjectsData

--- constants/ProjectsData.js
+++ constants/ProjectsData.js
@@ -1,29 +1,38 @@
 export const ProjectsData = [
     {
-        projectName: "Shovee : Personal Portfolio Generator",
-        liveUrl: "https://shovee.com",
-        githubUrl: "https://github.com/iamshiv007/shovee",
+        projectName: "Smart Door Lock Using FingerPrint Sensor",
+        liveUrl: "https://www.linkedin.com/posts/tu-bagus-dwi-fikri-27a3a0233_esp32-arduinoide-esp8266-activity-7234441378653433856-_0Hy?utm_source=share&utm_medium=member_desktop&rcm=ACoAADpBf34Bhpk4tWy7sWJeSC12ax-XQqXRidE",
+        githubUrl: "https://github.com/TuBagus7/FingerPrint_Succed.git",
         projectImage: {
-            imageUrl: "http://res.cloudinary.com/dqfrtazgi/image/upload/v1696218285/yn3naeznjfgszsbbjr92.png"
+            imageUrl: "/images/fingerprint.jpg"
         },
-        techs: ["NextJS", "TailwindCSS", "Redux Toolkit", "Cloudinary", "Intersection Objerver"]
+        techs: ["ESP32", "Fingerprint Sensor", "PHP", "MySQL", "Bootstrap"]
     },
     {
-        projectName: "YouTube clone",
-        liveUrl: "https://youtube-clone-shivraj.vercel.app",
-        githubUrl: "https://github.com/iamshiv007/yotube-clone",
+        projectName: "Line Follower Robot",
+        liveUrl: "https://www.linkedin.com/posts/tu-bagus-dwi-fikri-27a3a0233_the-basics-of-microcontrollers-are-important-activity-7231013443112202240-nuwc?utm_source=share&utm_medium=member_desktop&rcm=ACoAADpBf34Bhpk4tWy7sWJeSC12ax-XQqXRidE",
+        githubUrl: "https://github.com/TuBagus7/Line_Follower_Basic_Arduino",
         projectImage: {
-            imageUrl: "http://res.cloudinary.com/dqfrtazgi/image/upload/v1696218286/t1zjvlwogccwit9888w7.png"
+            imageUrl: "/images/car.png"
         },
-        techs: ["ReactJS", "ContextAPI", "ChakraUI", "YoutubeAPI", "ContextAPI"]
+        techs: ["Arduino Uno", "Dinamo Motor", "Light Sensor "]
     },
     {
-        projectName: "Eccommerce Application",
-        liveUrl: "https://ecommerce-application-shivraj.vercel.app",
-        githubUrl: "https://github.com/iamshiv007/ecommerce-frontend",
+        projectName: "Obstacle Avoiding Robot",
+        liveUrl: "https://www.linkedin.com/posts/tu-bagus-dwi-fikri-27a3a0233_obstacle-avoiding-car-for-more-https-activity-7233724154481418241-2Iaz?utm_source=share&utm_medium=member_desktop&rcm=ACoAADpBf34Bhpk4tWy7sWJeSC12ax-XQqXRidE",
+        githubUrl: "https://github.com/TuBagus7/Car_Obstacle_Arduino",
         projectImage: {
-            imageUrl: "http://res.cloudinary.com/dqfrtazgi/image/upload/v1696220173/lnnt6nmesjvvmo3nx9ep.png"
+            imageUrl: "/images/avoiding_car.png"
         },
-        techs: ["ReactJS", "NodeJS", "Express", "MongoDB", "Redux Toolkit"]
-    }
+        techs: ["Arduino Uno", "Dinamo Motor", "UltraSonic Sensor", "Light Sensor "]
+    },
+    {
+        projectName: "Non-Invasive Blood Glucose Meter",
+        liveUrl: "https://www.linkedin.com/posts/tu-bagus-dwi-fikri-27a3a0233_non-invasive-blood-glucose-meter-a-non-invasive-activity-7220509009886208000-Hf7A?utm_source=share&utm_medium=member_desktop&rcm=ACoAADpBf34Bhpk4tWy7sWJeSC12ax-XQqXRidE",
+        githubUrl: "https://github.com/TuBagus7/Blood_Sugar_Level_IoT_checked",
+        projectImage: {
+            imageUrl: "/images/invansive.jpg"   ,
+        },
+        techs: ["ESP32", "IoT", "Telegram Bot", "Infrared Sensor"]
+    },
 ]
